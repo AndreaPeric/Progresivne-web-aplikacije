733c57f66d64fb25502aa9715821cb564aefae91
AndreaPeric <72857976+AndreaPeric@users.noreply.github.com>
2021-10-30 22:50:43 +0200

    Dodan UI library

---
+++ src/components/Form.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <InputText type="text" v-model="value" placeholder="Username" />
+    <Button label="Submit" icon="pi pi-check" iconPos="right"/>
+  </form>  
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  Button {
+    margin-left:10px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/bootstrap4-light-purple/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
+const app = createApp(App);
+
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.use(PrimeVue);
+app.mount('#app')
